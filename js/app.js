d188a106d534c65b27ba6abb2df3cb34a2e4df7c
daniel chang <danielc718@gmail.com>
2018-02-28 11:07:26 -0500

    version 1

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function ($) {
+
+    particlesJS.load('particles-js', 'particlesjs/particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+
+});
