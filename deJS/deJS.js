36178eef6671aa06e6f18f1d7eec6c0b676b3226
daniel chang <danielc718@gmail.com>
2018-03-02 15:28:43 -0500

    version 2

---
+++ deJS/deJS.js
@@ -0,0 +1,53 @@
+const ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+const TIME = 50;
+var queue = [];
+let currentLetter = 0;
+let progress = 0;
+
+window.deJS = {};
+
+window.deJS.load = function() {
+    //get elements in the queue
+    $(".decodeEffect").each(function(){
+        queue.push(
+            {
+                element: $(this).get(0),
+                text: $(this).html()
+            }
+        );
+    });
+
+    //animate text
+    for (var i = 0; i < queue.length; i++) {
+        animate(queue[i]);
+    }
+}
+
+function animate(container) {
+
+    setTimeout(function(){
+        currentLetter++;
+        var currentText = container.text.substr(0, currentLetter);
+        currentText += getRandomChars(container.text.length - currentLetter);
+
+        container.element.innerHTML = currentText;
+        progress = currentLetter/container.text.length;
+
+        if(progress < 1) {
+          animate(container)
+        }
+    }, TIME);
+}
+
+function getRandomChars(size) {
+    var result = '';
+
+    for(var i = 0; i < size; i++) {
+        if(i % 5 == 0) {
+            result += ' '
+        } else {
+            result += ALPHA.charAt(Math.floor(Math.random() * ALPHA.length));
+        }
+    }
+    return result
+}
--- js/app.js
+++ js/app.js
@@ -4,4 +4,6 @@ $(document).ready(function ($) {
       console.log('callback - particles.js config loaded');
     });
 
+    deJS.load();
+
 });
